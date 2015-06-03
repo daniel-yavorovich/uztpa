d36fddc630fb4a8e00931740ee9b87d517a9cf95
Efimenko <efimenko-frontend@yandex.ru>
2015-06-04 00:54:38 +0300

    data-bg fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,26 @@
 $(function(){
+
 	$('#header-config').hover(function(){
-		$(this).find('ul').fadeIn();
-	},
-	function(){
-		$(this).find('ul').fadeOut();
+		$(this).find('ul').fadeToggle();
 	});
 
+	// Прокрутка вверх
 	$( ".toup" ).click(function(){
 		$('html, body').animate({scrollTop: 0}, 1000);
 	})
 
+	// Открытие подменю на моб версии
 	$('.car').click(function(e){
 		var menu = $(this).parent().siblings('.dropdown-menu');
 		$('.dropdown-menu').not(menu).parents('.dropdown').removeClass('open');
 		menu.parents('.dropdown').toggleClass('open')
-		// menu.slideToggle();
 		return false;
 	})
+
+	$('div[data-bg]').each(function(){
+		
+		var src = 'url('+$(this).attr('data-bg')+')';
+		console.log(src) 
+		$(this).css('background-image', src);
+	});
 });
