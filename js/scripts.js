1059d9372e99d04151e074281cdcd5237890e19e
Efimenko <efimenko-frontend@yandex.ru>
2015-06-03 03:33:14 +0300

    menu mobile

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,16 @@ $(function(){
 	function(){
 		$(this).find('ul').slideUp();
 	});
+	// $(window).resize(function(){
+	// 	// setTimeout(function(){
+	// 		$('#header-config').css({'float':'none','float':'right'});
+	// 	// },3000);
+		
+	// });
+
+	$('.car').click(function(e){
+		e.stopPropagation();
+		$('.dropdown-menu').slideUp();
+		$(this).parent().siblings('.dropdown-menu').slideToggle();
+	})
 });
