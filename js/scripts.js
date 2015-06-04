fb38756f0c1b779fe37f5327745e97302ee7664a
Efimenko <efimenko-frontend@yandex.ru>
2015-06-04 12:42:01 +0300

    full index

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,22 @@ $(function(){
 		return false;
 	})
 
+	// bg с html
 	$('div[data-bg]').each(function(){
-		
 		var src = 'url('+$(this).attr('data-bg')+')';
-		console.log(src) 
 		$(this).css('background-image', src);
+	});
+
+	// fancybox видео
+	$(".fancybox_video").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '70%',
+		height		: '70%',
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
 	});
 });
